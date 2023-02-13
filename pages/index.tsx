cf8727290a6e40a1cf4ec76f97c505c8c50b1ef0
Amol shelke <shelkeamol897@gmail.com>
2023-02-13 23:07:37 +0530

    Configured Tailwind css in NextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <section className="h-screen bg-blue-700">
+          <h1>Hello</h1>
+        </section>
+      </main>
     </div>
   );
 }
