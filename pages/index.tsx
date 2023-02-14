9d886ed943bd38fa26fdb4a3d4649dbfa10c7ced
Amol shelke <shelkeamol897@gmail.com>
2023-02-14 22:31:49 +0530

    Added styles for navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,16 +11,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className=" bg-white px-10">
         <section className="min-h-screen">
-          <nav>
-            <h1>AmolShelke</h1>
-            <ul>
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">AmolShelke</h1>
+            <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill />
+                <BsFillMoonStarsFill className="cursor-pointer text-xl" />
               </li>
               <li>
-                <a href="#">Resume</a>
+                <a
+                  href="#"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4  py-2 rounded-md ml-8">
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
